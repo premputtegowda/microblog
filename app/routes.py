542cc963dad2087e1afd7801f0788251c1a683e2
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-08-28 06:25:27 -0700

    added base and index templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 # first app is the main folder name
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello, World!'
+    user = {'username': 'prem'}
+    posts= [
+        { 
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland'
+        },
+        { 
+            'author': {'username': 'Suasan'},
+            'body': 'The Avengers movie was so cool'
+        }
+        ]
+    return render_template('index.html', title='Home' , user=user, posts = posts)
