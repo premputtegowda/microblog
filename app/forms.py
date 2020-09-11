ff6e3edf222464ce576bc9d61248ce0cc47d8a1c
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-09-11 11:20:41 -0700

    completed user profile

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired,Email, EqualTo, ValidationError
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired,Email, EqualTo, ValidationError, Length
 from app.models import User
 
 class LoginForm(FlaskForm):
@@ -10,21 +10,6 @@ class LoginForm(FlaskForm):
     signIn = SubmitField('Sign In')
 
 class RegistrationForm(FlaskForm):
-    # username = StringField('Username', validators=[DataRequired()])
-    # email = StringField('Email', validators = [Email()])
-    # password = PasswordField('Password', validators=[DataRequired()] )
-    # password2 = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')] )
-    # submit = SubmitField('Register')
-
-    # def validate_username(self, username):
-    #     user = User.query.filter_by(username=username).first()
-    #     if user is not None:
-    #         raise ValidationError('Please choose a different username')
-    
-    # def validate_email(self, email):
-    #     user = User.query.filter_by(username=email).first()
-    #     if user is not None:
-    #         raise ValidationError('Please choose a different email')
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -42,3 +27,7 @@ class RegistrationForm(FlaskForm):
         if user is not None:
             raise ValidationError('Please use a different email address.')
 		
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
