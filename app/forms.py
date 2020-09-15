de6a1cb46351a93ff7ee8361ff50096ea01bd785
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-09-15 15:21:06 -0700

    added many to many

--- app/forms.py
+++ app/forms.py
@@ -32,8 +32,8 @@ class EditProfileForm(FlaskForm):
     about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
     submit = SubmitField('Submit')
 
-    def __init__(self, original_username, *args, **kwargs):
-        super(EditProfileForm, self).__init__(*args, **kwargs)
+    def __init__(self, original_username):
+        super().__init__()
         self.original_username = original_username
 
     def validate_username(self, username):
