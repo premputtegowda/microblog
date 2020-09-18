4b7f81c764715db4a2297e6cd77afb6641c959c4
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-09-18 13:06:23 -0700

    added pagination

--- Config.py
+++ Config.py
@@ -12,6 +12,9 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['premkgowda@gmail.com']
+    POSTS_PER_PAGE=3
+
+   
 
     
     
