9931e049b779511351b33e93ca3b42335191a7a9
Prem Puttegowda <puttegowdaprem@gmail.com>
2020-09-20 19:36:20 -0700

    paginations to index

--- app/routes.py
+++ app/routes.py
@@ -91,13 +91,15 @@ def logout():
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
 
-    posts = [
-        {'author': user, 'body': 'Test post #1'},
-        {'author': user, 'body': 'Test post #2'}
+    page = request.args.get('page', 1, type=int)
+    posts = user.posts.order_by(Post.timestamp().desc()).Paginate(page,app.config['POSTS_PER_PAGE'], false)
 
-    ]
+    next_url = url_for('user', username=user.username, page=posts.next_num) \
+        if posts.has_next else None
+    prev_url = url_for('user', username=user.username, page=posts.prev_num) \
+        if posts.has_prev else None
 
-    return render_template('user.html', user = user, posts = posts)
+    return render_template('user.html', user = user, posts = posts, next_url=next_url, prev_url = prev_url)
 
 @app.route('/edit_profile', methods = ['GET', 'POST'])
 @login_required
